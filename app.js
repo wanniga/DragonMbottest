d2c7e95a8a736d3336c48fe8b9ad033cbda15da0
wanniga <41385413+wanniga@users.noreply.github.com>
2018-07-23 08:10:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var request = require('request')
 const thtime = require("./thtime")
 
-var TOKEN = process.env.BOT_TOKEN1
+var TOKEN = process.env.BOT_TOKEN
 
 setInterval(() => timechk(), 10000);
 
@@ -17,7 +17,7 @@ function aler1() {
     var s = thtime().s
 
     // if (m ==  && s <= 10) { sendText() }                   //แก้เวลาตรงนี้ สำหรับข้อความ "บอสโลกเกิดแล้ว"  แจ้งเตือน .00 น
-    if (h == 08 && m == 09 && s <= 10) { sendText() }        //แจ้งเตือนตอนเวลา 17.00 น
+    if (h == 12 && m == 30 && s <= 10) { sendText() }        //แจ้งเตือนตอนเวลา 17.00 น
 
 }
 
@@ -28,16 +28,6 @@ function aler2() {
 
     // if (m == 42 && s <= 10) { sendText15() }                //แก้เวลาตรงนี้ สำหรับข้อความ "อีก 15 บอสโลกเกิด"   แจ้งเตือน .45 น
     if (h == 12 && m == 15 && s <= 10) { sendText15() }     //แจ้งเตือนตอนเวลา 16.45 น
-
-}
-
-function aler3() {
-    var h = thtime().h
-    var m = thtime().m
-    var s = thtime().s
-
-    // if (m ==  && s <= 10) { sendText() }                   //แก้เวลาตรงนี้ สำหรับข้อความ "วอร์"  แจ้งเตือน .00 น
-    if (h == 20 && m == 00 && s <= 10) { sendText1() }        //แจ้งเตือนตอนเวลา 17.00 น
 
 }
 
@@ -75,28 +65,6 @@ function sendText15() {
         },
         form: {
             message: 'อีก 15 นาที บอสโลกจะเกิดแล้วนะ !!!', //ข้อความที่จะส่ง
-        },
-    }, (err, httpResponse, body) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(body)
-        }
-    })
-}
-
-function sendText1() {
-    request({
-        method: 'POST',
-        uri: 'https://notify-api.line.me/api/notify',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-            bearer: TOKEN, //token
-        },
-        form: {
-            message: 'วอร์จ้าาา !!!', //ข้อความที่จะส่ง
         },
     }, (err, httpResponse, body) => {
         if (err) {
