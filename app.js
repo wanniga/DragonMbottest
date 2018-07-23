680c7cf9bd49236cd9ae1e15ab362e4f8a2591e1
wanniga <41385413+wanniga@users.noreply.github.com>
2018-07-23 10:04:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ function alert() {
     var day = thtime().day
     var msgz1 = 'อีก 15 นาที บอสเกิดแล้ว'
     var msgz2 = 'บอสโลกเกิดแล้ว'
-    var msgz3 = 'ขนะนี้เวลา :'+day+' '+h+' '+m
+    var msgz3 = 'กิจกรรมลอกข้อสอบกิล :'+day+' '+h+' '+m
 
-    if (h == 09 && m == 59 && s <= 10) {            //เวลา
-         sendText(msgz1)                            //บอสโลกเกิดแล้ว
+    if (h == 12 && m == 15 && s <= 10) {            //เวลา
+         sendText(msgz1)                            //อีก 15 นาที บอสเกิดแล้ว
         }    
 
     if (h == 09 && m == 58 && s <= 10) {
-         sendText(msgz2)                            //อีก 15 นาที บอสเกิดแล้ว
+         sendText(msgz2)                            //บอสโลกเกิดแล้ว
         }
 
-    if (day == 1 && h == 09 && m == 60 && s <= 10) { 
-        sendText(msgz3) 
+    if (day == 1 && h == 10 && m == 06 && s <= 10) { 
+        sendText(msgz3)                             //ลอกข้อสอบ
     }  
 }
 
