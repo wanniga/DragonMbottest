07137b7b50306f5d267291ea6b003cc5de77364e
wanniga <41385413+wanniga@users.noreply.github.com>
2018-07-23 09:56:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,32 @@
 var request = require('request')
 const thtime = require("./thtime")
-
 var TOKEN = process.env.BOT_TOKEN1
 
-setInterval(() => timechk(), 10000);
+setInterval(() => alert(), 10000);
 
-function timechk() {
-    aler1()
-    aler2()
-    aler3()
-}
-
-function aler1() {
+function alert() {
     var h = thtime().h
     var m = thtime().m
     var s = thtime().s
     var day = thtime().day
+    var msgz1 = 'อีก 15 นาที บอสเกิดแล้ว'
+    var msgz2 = 'บอสโลกเกิดแล้ว'
+    var msgz3 = 'ขนะนี้เวลา :'+day+' '+h+' '+m
 
-    if (h == 09 && m == 51 && s <= 10) { sendText() }        //แจ้งเตือนตอนเวลา 17.00 น
-    if (h == 09 && m == 52 && s <= 10) { sendText15() }     //แจ้งเตือนตอนเวลา 16.45 น
-    if (day == 1 && h == 09 && m == 53 && s <= 10) { sendText1() //
-}
-function sendText() {
-    request({
-        method: 'POST',
-        uri: 'https://notify-api.line.me/api/notify',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-            bearer: TOKEN, //token
-        },
-        form: {
-            message: 'บอสโลกเกิดจ้าาาาาา !!!', //ข้อความที่จะส่ง
-        },
-    }, (err, httpResponse, body) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(body)
+    if (h == 09 && m == 59 && s <= 10) {            //เวลา
+         sendText(msgz1)                            //บอสโลกเกิดแล้ว
+        }    
+
+    if (h == 09 && m == 58 && s <= 10) {
+         sendText(msgz2)                            //อีก 15 นาที บอสเกิดแล้ว
         }
-    })
+
+    if (day == 1 && h == 09 && m == 60 && s <= 10) { 
+        sendText(msgz3) 
+    }  
 }
 
-function sendText15() {
+function sendText(msgz) {
     request({
         method: 'POST',
         uri: 'https://notify-api.line.me/api/notify',
@@ -54,29 +37,7 @@ function sendText15() {
             bearer: TOKEN, //token
         },
         form: {
-            message: 'อีก 15 นาที บอสโลกจะเกิดแล้วนะ !!!', //ข้อความที่จะส่ง
-        },
-    }, (err, httpResponse, body) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(body)
-        }
-    })
-}
-
-function sendText1() {
-    request({
-        method: 'POST',
-        uri: 'https://notify-api.line.me/api/notify',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-            bearer: TOKEN, //token
-        },
-        form: {
-            message: 'กิจกรรมลอกข้อสอบกิล !!!', //ข้อความที่จะส่ง
+            message: msgz, //ข้อความที่จะส่ง
         },
     }, (err, httpResponse, body) => {
         if (err) {
